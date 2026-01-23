954a9a096ecfc1ff59b31c94d5f639a9ea0fa5c9
Banduty <bandutysmods@gmail.com>
2026-01-23 19:10:26 +0100

    Alpha-15

--- forge/src/main/java/banduty/stoneycore/StoneyCoreForgeClient.java
+++ forge/src/main/java/banduty/stoneycore/StoneyCoreForgeClient.java
@@ -4,6 +4,10 @@ import banduty.stoneycore.block.CraftmanAnvilBlockRenderer;
 import banduty.stoneycore.block.ModBlockEntities;
 import banduty.stoneycore.client.SCAccessoryItemRenderer;
 import banduty.stoneycore.items.armor.SCAccessoryItem;
+import banduty.stoneycore.model.UnderArmourBootsModel;
+import banduty.stoneycore.model.UnderArmourChestplateModel;
+import banduty.stoneycore.model.UnderArmourHelmetModel;
+import banduty.stoneycore.model.UnderArmourLeggingsModel;
 import banduty.stoneycore.platform.ForgeHumanoidModelSetupAnimHelper;
 import banduty.stoneycore.platform.ForgeKeyInputHelper;
 import banduty.stoneycore.platform.ForgeRenderFirstPersonAccessoryArmorHelper;
@@ -14,6 +18,7 @@ import io.wispforest.accessories.api.client.AccessoriesRendererRegistry;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -36,5 +41,13 @@ public class StoneyCoreForgeClient {
                     .filter(item -> item instanceof SCAccessoryItem)
                     .forEach(item -> AccessoriesRendererRegistry.registerRenderer(item, SCAccessoryItemRenderer::new));
         });
+    }
+
+    @SubscribeEvent
+    public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(UnderArmourHelmetModel.LAYER_LOCATION, UnderArmourHelmetModel::getTexturedModelData);
+        event.registerLayerDefinition(UnderArmourChestplateModel.LAYER_LOCATION, UnderArmourChestplateModel::getTexturedModelData);
+        event.registerLayerDefinition(UnderArmourLeggingsModel.LAYER_LOCATION, UnderArmourLeggingsModel::getTexturedModelData);
+        event.registerLayerDefinition(UnderArmourBootsModel.LAYER_LOCATION, UnderArmourBootsModel::getTexturedModelData);
     }
 }
