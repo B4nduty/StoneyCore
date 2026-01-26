3deaddb590e5501110810127d0dc172f1011b056
Banduty <bandutysmods@gmail.com>
2026-01-26 22:06:40 +0100

    Alpha-30

--- common/src/main/java/banduty/stoneycore/util/definitionsloader/AccessoriesDefinitionData.java
+++ common/src/main/java/banduty/stoneycore/util/definitionsloader/AccessoriesDefinitionData.java
@@ -6,13 +6,13 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 
 public record AccessoriesDefinitionData(double armor, double toughness, String armorSlot, double hungerDrainMultiplier,
-                                        double deflectChance, double weight, ResourceLocation visoredHelmet) {
+                                        float deflectChance, double weight, ResourceLocation visoredHelmet) {
     public static final Codec<AccessoriesDefinitionData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.DOUBLE.optionalFieldOf("armor", 0.0).forGetter(AccessoriesDefinitionData::armor),
             Codec.DOUBLE.optionalFieldOf("toughness", 0.0).forGetter(AccessoriesDefinitionData::toughness),
             Codec.STRING.optionalFieldOf("armorSlot", "").forGetter(AccessoriesDefinitionData::armorSlot),
             Codec.DOUBLE.optionalFieldOf("hungerDrainMultiplier", 0.0).forGetter(AccessoriesDefinitionData::hungerDrainMultiplier),
-            Codec.DOUBLE.optionalFieldOf("deflectChance", 0.0).forGetter(AccessoriesDefinitionData::deflectChance),
+            Codec.FLOAT.optionalFieldOf("deflectChance", 0.0f).forGetter(AccessoriesDefinitionData::deflectChance),
             Codec.DOUBLE.optionalFieldOf("weight", 0.0).forGetter(AccessoriesDefinitionData::weight),
             ResourceLocation.CODEC.optionalFieldOf("visoredHelmet", new ResourceLocation("", "")).forGetter(AccessoriesDefinitionData::visoredHelmet)
     ).apply(instance, AccessoriesDefinitionData::new));
--- common/src/main/java/banduty/stoneycore/util/definitionsloader/ArmorDefinitionData.java
+++ common/src/main/java/banduty/stoneycore/util/definitionsloader/ArmorDefinitionData.java
@@ -5,12 +5,12 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import java.util.Map;
 
-public record ArmorDefinitionData(Map<String, Double> damageResistance, double deflectChance, double weight) {
+public record ArmorDefinitionData(Map<String, Double> damageResistance, float deflectChance, double weight) {
     public static final Codec<ArmorDefinitionData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.unboundedMap(Codec.STRING.xmap(String::toUpperCase, s -> s), Codec.DOUBLE)
                     .optionalFieldOf("damageResistance", Map.of())
                     .forGetter(ArmorDefinitionData::damageResistance),
-            Codec.DOUBLE.optionalFieldOf("deflectChance", 0.0)
+            Codec.FLOAT.optionalFieldOf("deflectChance", 0.0f)
                     .forGetter(ArmorDefinitionData::deflectChance),
             Codec.DOUBLE.optionalFieldOf("weight", 0.0)
                     .forGetter(ArmorDefinitionData::weight)
