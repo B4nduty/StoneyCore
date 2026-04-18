a95aa172004b57429b43681af9783eb06240e8a3
Banduty <bandutysmods@gmail.com>
2026-04-19 00:17:46 +0200

    Fixed attacks doesn't work and slashing damage type consumes half of the stamina

--- fabric/src/main/java/banduty/stoneycore/networking/packet/AttackC2SPacket.java
+++ fabric/src/main/java/banduty/stoneycore/networking/packet/AttackC2SPacket.java
@@ -1,6 +1,7 @@
 package banduty.stoneycore.networking.packet;
 
 import banduty.stoneycore.StoneyCore;
+import banduty.stoneycore.combat.melee.SCDamageType;
 import banduty.stoneycore.util.WeightUtil;
 import banduty.stoneycore.util.data.playerdata.StaminaData;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
@@ -8,10 +9,15 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.item.ItemStack;
 
 public class AttackC2SPacket {
     public static void receive(MinecraftServer server, ServerPlayer player, ServerGamePacketListenerImpl handler,
                                FriendlyByteBuf buf, PacketSender responseSender) {
-        StaminaData.removeStamina(player, StoneyCore.getConfig().combatOptions().attackStaminaConstant() * WeightUtil.getCachedWeight(player));
+        ItemStack mainHandStack = player.getMainHandItem();
+        SCDamageType damageType = SCDamageType.determine(mainHandStack, player);
+        double stamina = StoneyCore.getConfig().combatOptions().attackStaminaConstant();
+        if (damageType == SCDamageType.SLASHING)  stamina *= 0.5;
+        StaminaData.removeStamina(player, stamina * WeightUtil.getCachedWeight(player));
     }
 }
--- forge/src/main/java/banduty/stoneycore/networking/packet/AttackC2SPacket.java
+++ forge/src/main/java/banduty/stoneycore/networking/packet/AttackC2SPacket.java
@@ -1,10 +1,12 @@
 package banduty.stoneycore.networking.packet;
 
 import banduty.stoneycore.StoneyCore;
+import banduty.stoneycore.combat.melee.SCDamageType;
 import banduty.stoneycore.util.WeightUtil;
 import banduty.stoneycore.util.data.playerdata.StaminaData;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.network.NetworkEvent;
 
 import java.util.function.Supplier;
@@ -14,7 +16,11 @@ public record AttackC2SPacket() {
         ctx.get().enqueueWork(() -> {
             ServerPlayer player = ctx.get().getSender();
             if (player != null) {
-                StaminaData.removeStamina(player, StoneyCore.getConfig().combatOptions().attackStaminaConstant() * WeightUtil.getCachedWeight(player));
+                ItemStack mainHandStack = player.getMainHandItem();
+                SCDamageType damageType = SCDamageType.determine(mainHandStack, player);
+                double stamina = StoneyCore.getConfig().combatOptions().attackStaminaConstant();
+                if (damageType == SCDamageType.SLASHING)  stamina *= 0.5;
+                StaminaData.removeStamina(player, stamina * WeightUtil.getCachedWeight(player));
             }
         });
         ctx.get().setPacketHandled(true);
