1261c22a1ecc9490363ea54b8cf130387dca5e65
Banduty <bandutysmods@gmail.com>
2026-06-20 18:49:01 +0200

    Fixed crash when siege throws projectile

--- common/src/main/java/banduty/stoneycore/entity/custom/AbstractSiegeProjectile.java
+++ common/src/main/java/banduty/stoneycore/entity/custom/AbstractSiegeProjectile.java
@@ -14,6 +14,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
@@ -30,7 +31,7 @@ public abstract class AbstractSiegeProjectile extends AbstractArrow {
 
     @Override
     protected ItemStack getPickupItem() {
-        return ItemStack.EMPTY;
+        return new ItemStack(Items.ARROW);
     }
 
     @Override
