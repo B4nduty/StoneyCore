19a7bf5840583e1781aa6d4912a8ad162533baef
phuccom000 <gdtitans6@gmail.com>
2026-06-22 10:44:53 +0700

    Fixed crash

--- common/src/main/java/banduty/stoneycore/util/data/itemdata/ItemStackHolder.java
+++ common/src/main/java/banduty/stoneycore/util/data/itemdata/ItemStackHolder.java
@@ -6,18 +6,20 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.item.ItemStack;
 
 public record ItemStackHolder(ItemStack stack) {
-    public static final Codec<ItemStackHolder> CODEC = ItemStack.CODEC
-            .xmap(ItemStackHolder::new, ItemStackHolder::stack);
 
-    public static final StreamCodec<RegistryFriendlyByteBuf, ItemStackHolder> STREAM_CODEC = ItemStack.STREAM_CODEC
-            .map(ItemStackHolder::new, ItemStackHolder::stack);
+    public static final Codec<ItemStackHolder> CODEC =
+            ItemStack.OPTIONAL_CODEC
+                    .xmap(ItemStackHolder::new, ItemStackHolder::stack);
+
+    public static final StreamCodec<RegistryFriendlyByteBuf, ItemStackHolder> STREAM_CODEC =
+            ItemStack.OPTIONAL_STREAM_CODEC
+                    .map(ItemStackHolder::new, ItemStackHolder::stack);
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ItemStackHolder that = (ItemStackHolder) o;
-        return ItemStack.matches(this.stack, that.stack);
+        if (!(o instanceof ItemStackHolder(ItemStack stack1))) return false;
+        return ItemStack.matches(this.stack, stack1);
     }
 
     @Override
