42870e5d3d8584c9bfc52d8483210643397c1fab
Banduty <bandutysmods@gmail.com>
2026-06-25 19:32:32 +0200

    Added a check

--- common/src/main/java/banduty/stoneycore/entity/custom/AbstractSiegeEntity.java
+++ common/src/main/java/banduty/stoneycore/entity/custom/AbstractSiegeEntity.java
@@ -256,8 +256,8 @@ public abstract class AbstractSiegeEntity extends LivingEntity {
     }
 
     private void updateTimers() {
-        setCooldown(getCooldown() - 1);
-        setReloadTime(getReloadTime() - 1);
+        if (getCooldown() > 0) setCooldown(getCooldown() - 1);
+        if (getReloadTime() > 0) setReloadTime(getReloadTime() - 1);
     }
 
     protected void handleMovementSounds(ServerLevel serverLevel) {
